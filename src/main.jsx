2385d307f66a401b9a2625b595c1d2692c467caf
Nabeel <urs.nabeel1@gmail.com>
2025-07-25 23:33:58 +0530

    Reformat the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import Login from './Login.jsx';
-import Dashboard from './Dashboard.jsx';
-import ProtectedRoute from './ProtectedRoute.jsx'; // 🔐 Add this line
-import './index.css';
+// import App from './components/App.jsx';
+import Login from './components/Login.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx'; // 🔐 Add this line
+import './css/index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
